385c8d3b09894f1d446f5d237bbac858361f4521
sh963852741 <sh963852741@126.com>
2021-01-11 12:11:13 +0800

    加入了分工

--- src/pages/Modules/LectureManagement/Lecture.vue
+++ src/pages/Modules/LectureManagement/Lecture.vue
@@ -1,5 +1,21 @@
 <template>
-    <div/>
+    <i-row>
+        <i-card title="讲座列表">
+            <i-row>
+                <i-row type="flex" :gutter="16">
+                    <i-col>
+                        <i-button>新建讲座</i-button>
+                    </i-col>
+                    <i-col>
+                        <i-auto-complete></i-auto-complete>
+                    </i-col>
+                </i-row>
+            </i-row>
+            <i-row>
+                <!-- 这里是表格 -->
+            </i-row>
+        </i-card>
+    </i-row>
 </template>
 
 <script>
